3f93a18de236959d9d5c6770aa561aef85b46936
Ricardo Nicacio <ricardo_nicacio@hotmail.com>
2018-05-31 16:28:53 -0300

    organizing code

--- instance.tf
+++ instance.tf
@@ -17,6 +17,23 @@ resource "aws_instance" "public1" {
   user_data = "${data.template_cloudinit_config.cloudinit-example.rendered}"
 }
 
+# Public 20gb volumes Instance1
+resource "aws_ebs_volume" "ebs-volume-1" {
+  availability_zone = "eu-west-1a"
+  size              = 20
+  type              = "gp2"
+
+  tags {
+    Name = "extra volume data public1"
+  }
+}
+
+resource "aws_volume_attachment" "ebs-volume-1-attachment" {
+  device_name = "${var.INSTANCE_DEVICE_NAME}"
+  volume_id   = "${aws_ebs_volume.ebs-volume-1.id}"
+  instance_id = "${aws_instance.public1.id}"
+}
+
 #   Instance Public 2
 resource "aws_instance" "public2" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
@@ -34,6 +51,23 @@ resource "aws_instance" "public2" {
 
   # User Data
   user_data = "${data.template_cloudinit_config.cloudinit-example.rendered}"
+}
+
+# Public 20gb volumes Instance2
+resource "aws_ebs_volume" "ebs-volume-2" {
+  availability_zone = "eu-west-1b"
+  size              = 20
+  type              = "gp2"
+
+  tags {
+    Name = "extra volume data public2"
+  }
+}
+
+resource "aws_volume_attachment" "ebs-volume-2-attachment" {
+  device_name = "${var.INSTANCE_DEVICE_NAME}"
+  volume_id   = "${aws_ebs_volume.ebs-volume-2.id}"
+  instance_id = "${aws_instance.public2.id}"
 }
 
 #   Instance Public 3
@@ -55,6 +89,23 @@ resource "aws_instance" "public3" {
   user_data = "${data.template_cloudinit_config.cloudinit-example.rendered}"
 }
 
+# Public 20gb volumes Instance3
+resource "aws_ebs_volume" "ebs-volume-3" {
+  availability_zone = "eu-west-1c"
+  size              = 20
+  type              = "gp2"
+
+  tags {
+    Name = "extra volume data public3"
+  }
+}
+
+resource "aws_volume_attachment" "ebs-volume-3-attachment" {
+  device_name = "${var.INSTANCE_DEVICE_NAME}"
+  volume_id   = "${aws_ebs_volume.ebs-volume-3.id}"
+  instance_id = "${aws_instance.public3.id}"
+}
+
 #   Instance Private 1
 resource "aws_instance" "private1" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
@@ -72,6 +123,24 @@ resource "aws_instance" "private1" {
 
   # User Data
   user_data = "${data.template_cloudinit_config.cloudinit-example.rendered}"
+}
+
+# Private 20gb volume Instance 1
+
+resource "aws_ebs_volume" "ebs-volume-4" {
+  availability_zone = "eu-west-1a"
+  size              = 20
+  type              = "gp2"
+
+  tags {
+    Name = "extra volume data private1"
+  }
+}
+
+resource "aws_volume_attachment" "ebs-volume-4-attachment" {
+  device_name = "${var.INSTANCE_DEVICE_NAME}"
+  volume_id   = "${aws_ebs_volume.ebs-volume-4.id}"
+  instance_id = "${aws_instance.private1.id}"
 }
 
 #   Instance Private 2
@@ -93,6 +162,23 @@ resource "aws_instance" "private2" {
   user_data = "${data.template_cloudinit_config.cloudinit-example.rendered}"
 }
 
+# Private 20gb volume Instance 2
+resource "aws_ebs_volume" "ebs-volume-5" {
+  availability_zone = "eu-west-1b"
+  size              = 20
+  type              = "gp2"
+
+  tags {
+    Name = "extra volume data private2"
+  }
+}
+
+resource "aws_volume_attachment" "ebs-volume-5-attachment" {
+  device_name = "${var.INSTANCE_DEVICE_NAME}"
+  volume_id   = "${aws_ebs_volume.ebs-volume-5.id}"
+  instance_id = "${aws_instance.private2.id}"
+}
+
 #   Instance Private 3
 resource "aws_instance" "private3" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
@@ -112,100 +198,18 @@ resource "aws_instance" "private3" {
   user_data = "${data.template_cloudinit_config.cloudinit-example.rendered}"
 }
 
-# Public 20gb volumes Instances
-resource "aws_ebs_volume" "ebs-volume-1" {
-  availability_zone = "eu-west-1a"
-  size              = 20
-  type              = "gp2"
-
-  tags {
-    Name = "extra volume data"
-  }
-}
-
-resource "aws_volume_attachment" "ebs-volume-1-attachment" {
-  device_name = "${var.INSTANCE_DEVICE_NAME}"
-  volume_id   = "${aws_ebs_volume.ebs-volume-1.id}"
-  instance_id = "${aws_instance.public1.id}"
-}
-
-resource "aws_ebs_volume" "ebs-volume-2" {
-  availability_zone = "eu-west-1b"
-  size              = 20
-  type              = "gp2"
-
-  tags {
-    Name = "extra volume data"
-  }
-}
-
-resource "aws_volume_attachment" "ebs-volume-2-attachment" {
-  device_name = "${var.INSTANCE_DEVICE_NAME}"
-  volume_id   = "${aws_ebs_volume.ebs-volume-2.id}"
-  instance_id = "${aws_instance.public2.id}"
-}
-
-resource "aws_ebs_volume" "ebs-volume-3" {
+# Private 20gb volume Instance 3
+resource "aws_ebs_volume" "ebs-volume-6" {
   availability_zone = "eu-west-1c"
   size              = 20
   type              = "gp2"
 
   tags {
-    Name = "extra volume data"
+    Name = "extra volume data private3"
   }
 }
 
-resource "aws_volume_attachment" "ebs-volume-3-attachment" {
-  device_name = "${var.INSTANCE_DEVICE_NAME}"
-  volume_id   = "${aws_ebs_volume.ebs-volume-3.id}"
-  instance_id = "${aws_instance.public3.id}"
-}
-
-# Private 20gb volumes Instances
-
-resource "aws_ebs_volume" "ebs-volume-1" {
-  availability_zone = "eu-west-1a"
-  size              = 20
-  type              = "gp2"
-
-  tags {
-    Name = "extra volume data"
-  }
-}
-
-resource "aws_volume_attachment" "ebs-volume-1-attachment" {
-  device_name = "${var.INSTANCE_DEVICE_NAME}"
-  volume_id   = "${aws_ebs_volume.ebs-volume-4.id}"
-  instance_id = "${aws_instance.private1.id}"
-}
-
-resource "aws_ebs_volume" "ebs-volume-2" {
-  availability_zone = "eu-west-1b"
-  size              = 20
-  type              = "gp2"
-
-  tags {
-    Name = "extra volume data"
-  }
-}
-
-resource "aws_volume_attachment" "ebs-volume-2-attachment" {
-  device_name = "${var.INSTANCE_DEVICE_NAME}"
-  volume_id   = "${aws_ebs_volume.ebs-volume-5.id}"
-  instance_id = "${aws_instance.private2.id}"
-}
-
-resource "aws_ebs_volume" "ebs-volume-3" {
-  availability_zone = "eu-west-1c"
-  size              = 20
-  type              = "gp2"
-
-  tags {
-    Name = "extra volume data"
-  }
-}
-
-resource "aws_volume_attachment" "ebs-volume-3-attachment" {
+resource "aws_volume_attachment" "ebs-volume-6-attachment" {
   device_name = "${var.INSTANCE_DEVICE_NAME}"
   volume_id   = "${aws_ebs_volume.ebs-volume-6.id}"
   instance_id = "${aws_instance.private3.id}"
